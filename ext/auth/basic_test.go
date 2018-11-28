f1e17a24937f97687412d110f5587d5011a3ada7
kozhevin <kozhevin.dima@gmail.com>
2018-11-28 21:06:40 +0300

    Some imports have been changed

--- ext/auth/basic_test.go
+++ ext/auth/basic_test.go
@@ -14,8 +14,8 @@ import (
 	"sync/atomic"
 	"testing"
 
-	"github.com/elazarl/goproxy"
-	"github.com/elazarl/goproxy/ext/auth"
+	"github.com/DimaKoz/goproxy"
+	"github.com/DimaKoz/goproxy/ext/auth"
 )
 
 type ConstantHanlder string
